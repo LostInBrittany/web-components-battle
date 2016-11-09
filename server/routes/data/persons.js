e2653b255a788c496fe533fc72ce42709d640ff4
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2016-11-09 13:36:59 +0100

    Server local images

--- server/routes/data/persons.js
+++ server/routes/data/persons.js
@@ -3,7 +3,7 @@
 exports.peoples = [
     {
         "id": "5763cd4d9d2a4f259b53c901",
-        "photo": "https://127.0.0.1:3001/img/59.jpg",
+        "photo": "http://127.0.0.1:3001/img/59.jpg",
         "firstname": "Leanne",
         "lastname": "Woodard",
         "entity": "BIOSPAN",
